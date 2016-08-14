49208eb9a139f7e6ce65351ba7c6f65c4d05164e
Richard Schulte <richard.j.schulte@gmail.com>
2016-08-14 15:25:49 -0400

    readme, config switch, multi args

--- Readme.md
+++ Readme.md
@@ -1,4 +1,108 @@
+## Intro
+
+Allows one to create an HTTP server around multiple python scripts. Great for doing computations and such with numpy/scipy, etc
+
+Credit to @astenta and @egassama for providing the python scripts as part of this challenge
+
 ## Getting Started
 
 1. ``npm install``
 2. ``npm start``
+
+## Requirements
+
+* Python 2.7
+* Node 6.22
+* watchman (for development - brew install watchman)
+
+## Add scripts
+
+1. Add the script to python/ directory
+2. Add it to scripts in config.toml:
+
+```js
+[[scripts]]
+singleArg = true # default false
+name = "rainfall"
+path = "Uniaxial_CycleCount/CalculateUniaxial.py"
+```
+
+## Python script behavior
+
+* should accept stdin arguments from sys.argv as strings or JSON strings
+* should print the results as a JSON string to stdout - print() seems to work fine
+
+Eventually, this could also be installed globally and executed with local commands
+
+## Making HTTP requests
+
+* can accept a single argument if the singleArg is set to true (helpful for sending an array as a single arg to the python script)
+
+
+### POST /calculate/rainfall
+
+**1. HTTP Request**
+```json
+[
+  [0,1,2,3,4,6],
+  [0,200,-100,300,-200,100],
+  [0,0.02,-0.01,0.03,-0.02,0.01]
+]
+```
+
+**2. Executes (behind the scenes)**
+```
+$: python python/Uniaxial_CycleCount/CalculateUniaxial.py "[[0,1,2,3,4,6], [0,200,-100,300,-200,100], [0,0.02,-0.01,0.03,-0.02,0.01]]"
+```
+**3.  Which Returns: (behind the scenes)**
+```
+[["6 to 1", 0.01, 200, 150], ["4 to 2", 0.01, 200, 0]]
+```
+**4. HTTP Response**
+
+```json
+[
+  [
+    "6 to 1",
+    0.01,
+    200,
+    150
+  ],
+  [
+    "4 to 2",
+    0.01,
+    200,
+    0
+  ]
+]
+```
+
+### POST /calculate/hyopthetical
+
+**Request**
+```json
+[
+  [0,1,2,3,4,6],
+  [0,200,-100,300,-200,100],
+  [0,0.02,-0.01,0.03,-0.02,0.01]
+]
+```
+
+**Response**
+
+```json
+[
+  [
+    "6 to 1",
+    0.01,
+    200,
+    150
+  ],
+  [
+    "4 to 2",
+    0.01,
+    200,
+    0
+  ]
+]
+```
--- index.js
+++ index.js
@@ -1,41 +1,47 @@
+import fs from "fs";
 import Koa from "koa";
 import convert from "koa-convert";
 import PythonShell from "python-shell";
 import body from "koa-better-body";
+import toml from "toml";
 
 import logger, { koaLogger } from "./logger";
-import config from "./config.json";
 
-const app = new Koa();
+const config = toml.parse(fs.readFileSync("./config.toml"));
+
 const router = require('koa-router')({ prefix: "/calculate" });
+const app = new Koa();
 const port = process.env.PORT || 9003;
+const { scripts } = config;
 
+app.use(convert(body()));
 app.use(convert(koaLogger));
 
-async function runScript(data, script) {
-  return new Promise((res, rej)=>{
-    PythonShell.run(`python/${script.path}`, {mode: 'text', args: [JSON.stringify(data)]}, (e, results) => {
-      if (e) rej(e);
-      else {
-        res(JSON.parse(results));
-      }
+async function runScript(script, data) {
+  return new Promise((res, rej)=> {
+    const postData = script.singleArg ? [JSON.stringify(data)] : [data.map((item) => JSON.stringify(data))];
+    PythonShell.run(`python/${script.path}`, {args: postData}, (e, results) => {
+       if (e) rej(e);
+       else {
+          res(JSON.parse(results));
+       }
     });
   });
 }
 
-for (var i = 0; i < config.scripts.length; i++) {
-  const script = config.scripts[i];
+for (var i = 0; i < scripts.length; i++) {
+  const script = scripts[i];
   router.post(`/${script.name}`, async(ctx, next) => {
     try {
-      ctx.body = await runScript(ctx.body, script);
+      ctx.body = await runScript(script, ctx.body);
       ctx.status = 200;
       logger.info(`${script.name} script executed successfully`);
-      await next(); // next is now a function
+      await next();
     } catch (err) {
       ctx.body = { message: err.message };
       ctx.status = err.status || 500;
       logger.error(`${script.name} script failed with ${err.message}`)
-      await next(); // next is now a function
+      await next();
     }
   });
 }
